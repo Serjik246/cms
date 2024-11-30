ed11c22a04d497dff2865ec144a89bb088a3ea05
Sergey <sergey.khalaev@gmail.com>
2024-12-01 02:24:36 +0500

    Исправление задания для урока 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const modalInputFieldName = document.querySelector('.modal__label_name');
-const modalInputDescription = document.querySelector('.modal__label_description');
+const modalInputFieldName = document.querySelector('.modal__label_name .modal__input');
+const modalInputDescription = document.querySelector('.modal__label_description .modal__input');
 const modalInputDiscountCheckbox = document.querySelector('.modal__checkbox-wrapper .modal__checkbox');
 const modalInputDiscountText = document.querySelector('.modal__checkbox-wrapper .modal__input_discount');
 

874dd48b4881e09bf92ed07ec850873219738c36
Sergey <sergey.khalaev@gmail.com>
2024-11-30 21:58:16 +0500

    Создана ветка lesson_2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const modalInputFieldName = document.querySelector('.modal__label_name');
+const modalInputDescription = document.querySelector('.modal__label_description');
+const modalInputDiscountCheckbox = document.querySelector('.modal__checkbox-wrapper .modal__checkbox');
+const modalInputDiscountText = document.querySelector('.modal__checkbox-wrapper .modal__input_discount');
+
+console.log(modalInputFieldName);
+console.log(modalInputDescription);
+console.log(modalInputDiscountCheckbox);
+console.log(modalInputDiscountText);

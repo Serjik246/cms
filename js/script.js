10fc8fe50a067e96f52812a22c2b4d253a26516b
Sergey <sergey.khalaev@gmail.com>
2024-12-02 03:15:23 +0500

    Первый коммит по заданию темы 3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
 'use strict'
 
-const modalTitle = document.querySelector('.modal_top .modal__title');
-const modalForm = document.querySelector('.modal__form');
-const modalDiscountCheckbox = document.querySelector('.modal__checkbox-wrapper .modal__checkbox');
-const modalDiscountInputText = document.querySelector('.modal__checkbox-wrapper .modal__input_discount');
+// 1 задание
+const statusOverlay = document.querySelector('.overlay');
 
-console.log(modalTitle);
-console.log(modalForm);
-console.log(modalDiscountCheckbox);
-console.log(modalDiscountInputText);
+statusOverlay.classList.remove('active');
+
+// 2 задание
+
+const test_object = {
+    id: 24601654816512,
+    title: 'Телевизор DEXP',
+    category: 'Техника для дома',
+    unit: 'шт',
+    count: 15,
+    price: 1000,
+
+}
+
+const createRow = (obj) => {
+    const row = document.createElement('tr');
+    
+};
+
+<tr>
+                <td class="table__cell ">2</td>
+                <td class="table__cell table__cell_left table__cell_name" data-id="24601654816512">
+                  <span class="table__cell-id">id: 24601654816512</span>Телевизор DEXP</td>
+                <td class="table__cell table__cell_left">Техника для дома</td>
+                <td class="table__cell">шт</td>
+                <td class="table__cell">15</td>
+                <td class="table__cell">$1000</td>
+                <td class="table__cell">$15000</td>
+                <td class="table__cell table__cell_btn-wrapper">
+                  <button class="table__btn table__btn_pic"></button>
+                  <button class="table__btn table__btn_edit"></button>
+                  <button class="table__btn table__btn_del"></button>
+                </td>
+              </tr>
